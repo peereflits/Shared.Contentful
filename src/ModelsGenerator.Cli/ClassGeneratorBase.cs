538b404cf659b2cf6e0717a679eaee861aab56e5
Kim Heurter <kim.heurter@incentro.com>
2024-08-23 14:38:04 +0200

    Added GeneratedContentTypeResolver and consolidated common logic into a base class. Ignored launchsettings.json, set environment default setting to a valid environment and upped the version.

---
+++ src/ModelsGenerator.Cli/ClassGeneratorBase.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Peereflits.Shared.Contentful.ModelsGenerator.Cli;
+public abstract class ClassGeneratorBase
+{
+    protected static string FormatClassName(string name) => FirstLetterToUpperCase(RemoveProhibitedCharacters(name));
+
+    protected static string FirstLetterToUpperCase(string word)
+    {
+        if (string.IsNullOrEmpty(word))
+        {
+            return word;
+        }
+
+        var a = word.ToCharArray();
+        a[0] = char.ToUpper(a[0]);
+        return new string(a);
+    }
+
+    private static string RemoveProhibitedCharacters(string word) =>
+        Regex.Replace(word, "[^A-Za-z0-9_]", string.Empty);
+}
